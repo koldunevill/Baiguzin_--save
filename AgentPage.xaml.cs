513b44ea6716e214e8db2928deceed141114b22d
Егор Байгузин <egorbayguzin05@mail.ru>
2023-11-06 14:54:56 +0500

    Лаба 11

--- AgentPage.xaml.cs
+++ AgentPage.xaml.cs
@@ -23,11 +23,93 @@ namespace Baiguzin_Глазки_save
         public AgentPage()
         {
             InitializeComponent();
+
+            var currentAgent = Baiguzin_glazkiEntities.GetContext().Agent.ToList();
+
+            AgentListView.ItemsSource = currentAgent;
+
+            ComboType.SelectedIndex = 0;
+            ComboType2.SelectedIndex = 0;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void ComboType2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();  
+        }
+        private void UpdateAgent()
+        {
+            var currentAgent = Baiguzin_glazkiEntities.GetContext().Agent.ToList();
+
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+
+            if (ComboType2.SelectedIndex == 0)
+            {
+                currentAgent = currentAgent;
+            }
+            if (ComboType2.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ЗАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МКК").ToList();
+            }
+            if (ComboType2.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+
+            currentAgent = currentAgent.Where(p => PhoneFormat(p.Phone.ToLower()).Trim().Contains(TBoxSearch.Text.ToLower()) || p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) 
+            ||  p.Email.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentAgent;
+
+        }
+        private string PhoneFormat(string phone)
+        {
+            return phone.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
         }
     }
 }
